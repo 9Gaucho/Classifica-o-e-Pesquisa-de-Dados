0e9ce04a468fe027ae03c53e7a3b5a0ad72b94d6
Samuel Watthier <98723071+9Gaucho@users.noreply.github.com>
2024-09-04 00:09:14 -0300

    Update MergeSort.py

--- MergeSort.py
+++ MergeSort.py
@@ -1,29 +1,40 @@
+import datetime
 def merge(v, ini, meio, fim):
-    tam = fim - ini + 1
     p1 = ini
     p2 = meio + 1
-    temp = []
+    tam = fim - ini + 1
+    temp = [0] * tam
+    i = 0
 
     while p1 <= meio and p2 <= fim:
         if v[p1] < v[p2]:
-            temp.append(v[p1])
+            temp[i] = v[p1]
             p1 += 1
         else:
-            temp.append(v[p2])
+            temp[i] = v[p2]
             p2 += 1
+        i += 1
 
     while p1 <= meio:
-        temp.append(v[p1])
+        temp[i] = v[p1]
         p1 += 1
+        i += 1
 
     while p2 <= fim:
-        temp.append(v[p2])
+        temp[i] = v[p2]
         p2 += 1
+        i += 1
 
-    for i in range(tam):
-        v[ini + i] = temp[i]
+    for j in range(tam):
+        v[ini + j] = temp[j]
 
-if __name__ == "__main__":
-    v = [38, 27, 43, 3, 9, 82, 10]
-    merge(v, 0, 3, 6) 
-    print(v)
+
+
+v = [1,6,3,4,5,8]
+tempoInicial = datetime.datetime.now()
+merge(v, 0, 2, 4)
+tempoFinal = datetime.datetime.now()
+soma = tempoFinal - tempoInicial
+print(soma)
+
+print(v)
