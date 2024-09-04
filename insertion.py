a34c9ea5ac7d053dae7ef684e2a92451b0fc10f6
Samuel Watthier <98723071+9Gaucho@users.noreply.github.com>
2024-09-04 00:09:38 -0300

    Update insertion.py

--- insertion.py
+++ insertion.py
@@ -1,16 +1,40 @@
-def insertion_sort(data):
-    n = len(data)
-    for j in range(1, n):
-        tmp = data[j]
-        i = j - 1
-        while i >= 0 and tmp < data[i]:
-            data[i + 1] = data[i]
-            i -= 1
-        data[i + 1] = tmp
+import datetime
+def merge(v, ini, meio, fim):
+    p1 = ini
+    p2 = meio + 1
+    tam = fim - ini + 1
+    temp = [0] * tam
+    i = 0
 
-# Exemplo de uso
-if __name__ == "__main__":
-    lista = [5, 2, 9, 1, 5, 6]
-    print("Lista original:", lista)
-    insertion_sort(lista)
-    print("Lista ordenada:", lista)
+    while p1 <= meio and p2 <= fim:
+        if v[p1] < v[p2]:
+            temp[i] = v[p1]
+            p1 += 1
+        else:
+            temp[i] = v[p2]
+            p2 += 1
+        i += 1
+
+    while p1 <= meio:
+        temp[i] = v[p1]
+        p1 += 1
+        i += 1
+
+    while p2 <= fim:
+        temp[i] = v[p2]
+        p2 += 1
+        i += 1
+
+    for j in range(tam):
+        v[ini + j] = temp[j]
+
+
+
+v = [1,6,3,4,5,8]
+tempoInicial = datetime.datetime.now()
+merge(v, 0, 2, 4)
+tempoFinal = datetime.datetime.now()
+soma = tempoFinal - tempoInicial
+print(soma)
+
+print(v)
